264ac934672a4d4880530aa22e3e13220df31815
Andre Kradolfer <narfdre@gmail.com>
2016-05-12 15:34:28 -0600

    SNAPSHOT

--- src/main/java/io/aiur/oss/db/jdbc/jdbc/binding/JdbcRepositoryFactory.java
+++ src/main/java/io/aiur/oss/db/jdbc/jdbc/binding/JdbcRepositoryFactory.java
@@ -1,23 +1,33 @@
 package io.aiur.oss.db.jdbc.jdbc.binding;
 
+import com.google.common.collect.Sets;
 import io.aiur.oss.db.jdbc.jdbc.JdbcRepository;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.core.GenericTypeResolver;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mapping.Association;
 import org.springframework.data.mapping.PersistentEntity;
+import org.springframework.data.mapping.PersistentProperty;
+import org.springframework.data.mapping.model.AnnotationBasedPersistentProperty;
 import org.springframework.data.mapping.model.BasicPersistentEntity;
+import org.springframework.data.mapping.model.SimpleTypeHolder;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
 import org.springframework.data.repository.core.support.TransactionalRepositoryFactoryBeanSupport;
 import org.springframework.data.util.ClassTypeInformation;
 import org.springframework.data.util.TypeInformation;
+import org.springframework.util.ReflectionUtils;
 
 import javax.inject.Inject;
+import java.beans.PropertyDescriptor;
 import java.io.Serializable;
 
 /**
  * Created by dave on 12/16/15.
  */
+@Slf4j
 public class JdbcRepositoryFactory<T extends Repository<S, ID>, S, ID extends Serializable> extends
         TransactionalRepositoryFactoryBeanSupport<T, S, ID> {
 
@@ -43,7 +53,31 @@ public class JdbcRepositoryFactory<T extends Repository<S, ID>, S, ID extends Se
     @Override
     public PersistentEntity<?, ?> getPersistentEntity() {
         TypeInformation<?> typeInfo = ClassTypeInformation.from(domainType);
-        return new BasicPersistentEntity(typeInfo);
+        BasicPersistentEntity basic = new BasicPersistentEntity(typeInfo);
+
+        // find the ID field
+        ReflectionUtils.doWithFields(domainType, (field)->{
+            if( basic.hasIdProperty() ){
+                log.warn("Found multiple ID properties for {}: {} and {}",
+                        domainType, basic.getIdProperty().getField().getType(), field.getType());
+            }else{
+                try {
+                    PropertyDescriptor pd = new PropertyDescriptor(field.getName(), domainType);
+                    SimpleTypeHolder sth = new SimpleTypeHolder(Sets.newHashSet(field.getType()), true);
+                    PersistentProperty prop = new AnnotationBasedPersistentProperty(field, pd, basic, sth) {
+                        @Override
+                        protected Association createAssociation() {
+                            return null;
+                        }
+                    };
+                    basic.addPersistentProperty(prop);
+                }catch(Exception e){
+                    log.warn("Failed adding PersistentProperty ID ", e);
+                }
+            }
+        }, (matchField) -> matchField.getDeclaredAnnotation(Id.class) != null);
+
+        return basic;
     }
 
     @Override
